d90288b8d38ee853c55bca811ed9052d0fd95434
SaosSambel <galihprakoso126@gmail.com>
2022-03-20 20:59:15 +0700

    server

--- src/server.js
+++ src/server.js
@@ -1,13 +1,15 @@
 console.log('Halo Semua');
 
 const Hapi = require('@hapi/hapi');
- 
- 
+const routes = require('./routes');
+
 const init = async () => {
   const server = Hapi.server({
     port: 5000,
     host: 'localhost',
   });
+
+  server.route(routes);
  
  
   await server.start();
